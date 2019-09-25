694fdd9c006e8d2da94757193b1e1ca5aaccc56d
fwplove <625881050@qq.com>
2019-09-25 16:45:45 +0800

    项目搭建

--- xc-service-api/src/main/java/com/xuecheng/api/CmsPageControllerApi.java
+++ xc-service-api/src/main/java/com/xuecheng/api/CmsPageControllerApi.java
@@ -2,7 +2,19 @@ package com.xuecheng.api;
 
 import com.xuecheng.framework.domain.cms.request.QueryPageRequest;
 import com.xuecheng.framework.model.response.QueryResponseResult;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
 
+
+@Api(value = "cms 页面管理接口",description = "cms 页面管理接口,提供页面的增,删,改,查")
 public interface CmsPageControllerApi {
+
+    @ApiOperation("分页查询页面列表")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "page",value = "页面",required = true,paramType = "path",dataType = "int"),
+            @ApiImplicitParam(name="size",value = "每页记录",required = true,paramType = "path",dataType = "int")
+    })
     public QueryResponseResult findList(int page, int size, QueryPageRequest queryPageRequest);
 }
